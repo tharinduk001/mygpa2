4527d45b80c3423abdaff9770d479c0fd553c825
AvishkaIndu <avishkafvr@gmail.com>
2025-06-18 12:00:37 +0530

    second init

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ekfudhddkmutvwlqwksb.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrZnVkaGRka211dHZ3bHF3a3NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0MTMwNzMsImV4cCI6MjA2Mzk4OTA3M30.7VVnrCPGz79xsDt6qIahKU--S3t0fjIifYa68gnd_zY';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase environment variables');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
